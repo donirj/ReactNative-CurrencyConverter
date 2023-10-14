ffb09581800068609c28ecac9bb8ebcd91760ec7
Andoni <andoniresendiz@gmail.com>
2023-10-14 14:25:57 -0600

    input components added

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,13 +1,69 @@
 import React from 'react'
 import { 
     View, 
-    TouchableOpacity, 
-    Text, 
-    SafeAreaView, 
+    StatusBar,
     StyleSheet,
-    ScrollView,
-    Linking,
-    Alert
+    Image,
+    Dimensions
 } from 'react-native'
 
-export default () => null;
+import colors from '../constants/colors'
+import { ConversionInput } from '../components/ConversionInput'
+
+const screen = Dimensions.get('window')
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.blue,
+        flex: 1,
+        justifyContent: "center"
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoBackground: {
+        
+        width: screen.width * 0.45,
+        height: screen.width * 0.45
+       
+    },
+    logo: {
+        position: 'absolute',
+        width: screen.width * 0.25,
+        height: screen.width * 0.25
+    }
+})
+
+export default () => {
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="light-content" backgroundColor={colors.blue}/>
+
+            <View style={styles.logoContainer}>
+                <Image 
+                    source={require('../../assets/background.png')} 
+                    style={styles.logoBackground}/>
+                <Image 
+                    source={require('../../assets/logo.png')} 
+                    style={styles.logo} 
+                    resizeMode='contain'/>
+                
+            </View>
+            <ConversionInput 
+                text="USD"
+                value="123"
+                onButtonPress={() => alert("todo")}
+                onChangeText={text => console.log('text', text)}
+                keyboardType="numeric"
+            />
+            <ConversionInput 
+                text="GBP"
+                value="123"
+                onButtonPress={() => alert("todo")}
+                editable={false}
+            />
+
+        </View>
+    )
+};
